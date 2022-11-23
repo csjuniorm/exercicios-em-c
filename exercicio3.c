9b0b690b5614f2ad486286a3475e9b0c5ceb2750
Sergio Moreira <64445781+juniormoreiradev@users.noreply.github.com>
2022-11-23 20:26:09 +0000

    Create exercicio3.c

---
+++ exercicio3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int num1;
+    
+    printf("Digite um numero: \n");
+    scanf("%d", &num1);
+    if (num1 < 20);
+    printf("O numero que digitou e inferior a 20");
+    
+return 0;
+}
